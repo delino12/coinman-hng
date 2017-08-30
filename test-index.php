2d720e0e77e348ba424725d7c33edccef5b047da
Nsikaktopdown <nsikakthompson73@gmail.com>
2017-08-31 00:13:57 +0100

    Added header

---
+++ test-index.php
@@ -0,0 +1,8 @@
+<?php include('header.php')?>
+<div class="data-section clearfix">
+    <div>
+    <div class="coin-1">
+    </div>    
+    <img class="image-size" src="./images/blue-coin.png" alt="">
+</div>
+</div>
